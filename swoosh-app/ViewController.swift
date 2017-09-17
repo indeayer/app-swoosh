d42b017e7bc5f906bb67e57cf1ace88cb595a9fa
Maurice <maurice@Maurices-MacBook-Pro.local>
2017-09-17 22:35:08 +0800

    Edited a D to words

--- swoosh-app/ViewController.swift
+++ swoosh-app/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.d
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreatedd.
     }
 
 
